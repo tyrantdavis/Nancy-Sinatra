ce0960b7e0c6331d073628b399b7e2a980cdaea1
Tyrant Davis <davistyrant@gmail.com>
2017-08-23 14:10:55 -0700

    Adds delegator module to delegate methods to Nancy::Application.

--- nancy.rb
+++ nancy.rb
@@ -47,6 +47,20 @@ module Nancy
       else
         [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
       end
+
+      module Delegator
+        def self.delegate(*methods, to:)
+          Array(methods).each do |method_name|
+            define_method(method_name) do |*args, &block|
+              to.send(method_name, *args, &block)
+            end
+
+            private method_name
+          end
+        end
+
+        delegate :get, :patch, :put, :post, :delete, :head, to: Application
+      end
     end
 
 
