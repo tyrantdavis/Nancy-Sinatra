f80b35907376e44034ecd08632a0603954160311
Tyrant Davis <davistyrant@gmail.com>
2017-08-23 08:34:53 -0700

    Implement Base class with a get method that takes a path and handler block.

---
+++ nancy.rb
@@ -0,0 +1,23 @@
+
+require "rack"
+
+module Nancy
+  class Base
+    def initialize
+      @routes = {}
+    end
+
+    attr_reader :routes
+
+    def get(path, &handler)
+      route("GET", path, &handler)
+    end
+
+    private
+
+    def route(verb, path, &handler)
+      @routes[verb] ||= {}
+      @routes[verb][path] = handler
+    end
+  end
+end
